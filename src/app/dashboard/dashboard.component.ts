6108655441036ece737cf2ce9e54d7f3b7cb916d
davidmadi <davidmadi@gmail.com>
2025-04-19 01:20:13 +0000

    theme

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
